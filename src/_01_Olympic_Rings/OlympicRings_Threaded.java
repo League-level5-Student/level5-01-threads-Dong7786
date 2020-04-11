3de889fc29cf28c9089320b7ca32bf4c5fb51ef9
Dong Zou <jiwenzou@gmail.com>
2020-04-11 15:48:13 -0700

    Finished Olympic Rings
    Started on the Advanced Robot Race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,40 +1,77 @@
 package _01_Olympic_Rings;
 
 import java.awt.Color;
-import java.awt.Graphics;
 
 import org.jointheleague.graphical.robot.Robot;
 
-public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+public class OlympicRings_Threaded{
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 3 robots.
 	public static void main(String[] args) {
 		OlympicRings_Threaded o = new OlympicRings_Threaded();
 		
 		
 	}
 	OlympicRings_Threaded(){
-		Robot blue = new Robot(200, 300);
-		Robot yellow = new Robot(400, 500);
-		Robot black = new Robot(600, 300);
-		Robot green = new Robot(800, 500);
-		Robot red = new Robot(1000, 300);
-		//blue.hide();
-		blue.setSpeed(3);
-		blue.setPenWidth(5);
-		blue.setPenColor(Color.blue);
+		Robot[] robot = new Robot[5];
+		robot[0] = new Robot(100, 400);
+			robot[0].setPenColor(Color.blue);
+		robot[1] = new Robot(400, 600);
+			robot[1].setPenColor(Color.yellow);
+		robot[2] = new Robot(700, 400);
+			robot[2].setPenColor(Color.black);
+		robot[3] = new Robot(1000, 600);
+			robot[3].setPenColor(Color.green);
+		robot[4] = new Robot(1300, 400);
+			robot[4].setPenColor(Color.red);
+		
+		for(int i = 0; i < robot.length; i ++) {
+			robot[i].hide();
+			robot[i].setPenWidth(50);
+			robot[i].penDown();
+		}
+		
+		Thread blue = new Thread(() -> {
+			for(int i = 0; i < 360; i ++) {
+				robot[0].move(4);
+				robot[0].turn(1);
+			}
+			});
+		Thread yellow = new Thread(() ->{
+			for(int i = 0; i < 360; i ++) {
+				robot[1].move(4);
+				robot[1].turn(1);
+			}
+			
+		});
+		Thread black = new Thread(() -> {
+			for(int i = 0; i < 360; i ++) {
+				robot[2].move(4);
+				robot[2].turn(1);
+				
+			}
+		});
+		Thread green = new Thread(()-> {
+			for(int i = 0; i < 360; i++) {
+				robot[3].move(4);
+				robot[3].turn(1);
+				
+			}
+		});
+		Thread red = new Thread(()-> {
+			for(int i = 0; i < 360; i ++) {
+				robot[4].move(4);
+				robot[4].turn(1);
+				
+			}
+		});
+		
+		blue.start();
+		yellow.start();
+		black.start();
+		green.start();
+		red.start();
+	}
 	
-		
-		Thread b = new Thread(() -> blue.move(1080));
-		Thread b1 = new Thread(() -> blue.turn(360));
-		
-		b.start();
-		b1.start();
-		
-	}
-	void draw(Graphics g) {
-		
-		
-	}
 	
 }
 
