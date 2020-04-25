ff55f573a24e07c20812c0f5aabf2def98c6a9c1
Dong Zou <jiwenzou@gmail.com>
2020-04-25 16:04:55 -0700

    Started the Task_Pool Project
    Finished the Task() Class and the ThreadPool() class.
    Haven't touched the Worker() Class but that would be next.

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,35 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool {
+Thread[] thread;
+ConcurrentLinkedQueue<Task> taskQueue;
+
+	ThreadPool(int totalThreads){
+		thread = new Thread[totalThreads];
+		for(int i = 0; i < thread.length; i ++) {
+			thread[i] = new Thread(new Worker());
+			
+		}
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+			
+	}
+	
+	void addTask(Task task) {
+		taskQueue.add(task);
+		
+	}
+	
+	void start() {
+		for(int i = 0; i < thread.length; i ++) {
+			thread[i].start();
+			try {
+				thread[i].join();
+			}catch(InterruptedException e) {
+				
+				
+			}
+		}
+	}
 }
